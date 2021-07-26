c4b3ebfa220929ccd75b92dcc4b16f5165752fc8
Victor de Sa <victordesadev@gmail.com>
2021-07-25 22:40:25 -0300

    alterando export da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/victordesa20/hc-desafio2-json'
-})
-
-export default api;
+export const api = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/victordesa20/hc-desafio2-json',
+});
