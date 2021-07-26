64a036bbbda37766418b9d14e13a2e733bc78974
HendrewMartins <46753193+HendrewMartins@users.noreply.github.com>
2021-07-26 16:18:15 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/victordesa20/hc-desafio2-json'
+  baseURL: 'https://my-json-server.typicode.com/victordesa20/hc-desafio2-json/produtos'
 })
 
 export default api;
